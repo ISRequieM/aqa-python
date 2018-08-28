70d1aa352a90f2acc58c02e1245c75340a63611d
ISRequieM <isrequiem@gmail.com>
2018-08-28 10:24:37 +0300

    Add one more test case

--- phase01/tests/testFibonacci.py
+++ phase01/tests/testFibonacci.py
@@ -4,3 +4,5 @@ from phase01.generateFibonacci import generateFibonacci
 def testFibonacci():
     expectedList = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
     assert generateFibonacci(15) == expectedList
+    expectedlist1 = [1, 1, 2, 3, 5]
+    assert generateFibonacci(5) == expectedlist1
