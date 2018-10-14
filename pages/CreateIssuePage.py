dfad2f96a6310e65fa34309b480950c6ea39aec7
ISRequieM <isrequiem@gmail.com>
2018-10-14 17:04:54 +0300

    Added test case for create issue

---
+++ pages/CreateIssuePage.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+class CreateIssuePage:
+    CREATE_BUTTON = (By.ID, "create_link")
+    PROJECT_FIELD = (By.ID, "project-field")
+    ISSUE_TYPE_FIELD = (By.ID, "issuetype-field")
+    SUMMARY_FIELD = (By.ID, "summary")
+    DESCRIPTION_FIELD = (By.ID, "tinymce")
+    SUBMIT_ISSUE_BUTTON = (By.ID, "create-issue-submit")
+    POPUP_CONTAINER = (By.ID, "aui-flag-container")
+
+    driver = None
+    wait = None
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def create_issue(self, project, issue_type, summary, description=None, priority=None, assignee=None):
+        create_button = self.wait.until(expected_conditions.element_to_be_clickable(self.CREATE_BUTTON))
+        create_button.click()
+        project_field = self.wait.until(expected_conditions.element_to_be_clickable(self.PROJECT_FIELD))
+        project_field.clear()
+        project_field.send_keys(project)
+        project_field.send_keys(Keys.RETURN)
+        issue_type_field = self.wait.until(expected_conditions.element_to_be_clickable(self.ISSUE_TYPE_FIELD))
+        issue_type_field.clear()
+        issue_type_field.send_keys(issue_type)
+        issue_type_field.send_keys(Keys.RETURN)
+        summary_field = self.wait.until(expected_conditions.element_to_be_clickable(self.SUMMARY_FIELD))
+        summary_field.clear()
+        summary_field.send_keys(summary)
+        submit_issue_button = self.wait.until(expected_conditions.element_to_be_clickable(self.SUBMIT_ISSUE_BUTTON))
+        submit_issue_button.click()
+        popup_container = self.wait.until(expected_conditions.visibility_of_element_located(self.POPUP_CONTAINER))
+        issue_key = popup_container.find_element(By.CLASS_NAME, "issue-created-key").get_attribute("data-issue-key")
+        self.wait.until(expected_conditions.invisibility_of_element(self.POPUP_CONTAINER))
+        return issue_key
+
+
--- tests/test_jiraUI.py
+++ tests/test_jiraUI.py
@@ -1,29 +1,52 @@
 from webdriver_manager.chrome import ChromeDriverManager
 
 from jira.jira import JiraParameters
+from pages.CreateIssuePage import CreateIssuePage
 from pages.LoginPage import LoginPage
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from jira.jira import JiraRestActions
 
 
-class TestJiraLogin:
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    #driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=chrome_options)
-    driver = webdriver.Chrome(options=chrome_options)
-    login_page = LoginPage(driver)
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+#driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(10)
+driver.set_page_load_timeout(10)
+driver.set_script_timeout(10)
+login_page = LoginPage(driver)
+create_page = CreateIssuePage(driver)
+issue_keys = {}
+user = JiraParameters.user
+password = JiraParameters.password
+project_key = JiraParameters.project_key
+rest_actions = JiraRestActions(user, password, project_key)
 
-    def test_jira_login_wrong_password(self):
-        assert self.login_page.login_to_jira_creds_privided(JiraParameters.user, "admin") == False
 
-    def test_jira_login_wrong_username(self):
-        assert self.login_page.login_to_jira_creds_privided("admin", JiraParameters.password) == False
+def test_jira_login_wrong_password():
+    assert login_page.login_to_jira_creds_privided(JiraParameters.user, "admin") == False
 
-    def test_jira_login(self):
-        assert self.login_page.login_to_jira()
 
-    def test_cleanup(self):
-        self.driver.quit()
+def test_jira_login_wrong_username():
+    assert login_page.login_to_jira_creds_privided("admin", JiraParameters.password) == False
+
+
+def test_jira_login():
+    assert login_page.login_to_jira() == True
+
+
+def test_create_issue():
+    issue_keys["0"] = create_page.create_issue(project="AQAPYTHON", issue_type="Bug", summary="some_summary")
+    assert issue_keys.__sizeof__() > 0
+    print(issue_keys.get("0"))
+
+
+def test_cleanup():
+    driver.quit()
+    assert rest_actions.authenticate() != "Failed"
+    assert rest_actions.delete_issue(issue_keys.get("0")).get("success") == True
+
 
 
 
